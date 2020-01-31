981057aa2369a55c1b51480aba9b23e24aad5936
Jacob Krevat <jacobtie@gmail.com>
2020-01-31 12:20:13 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace http_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+        }
+    }
+}
